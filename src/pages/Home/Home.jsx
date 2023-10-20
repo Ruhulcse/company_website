52b33b90dd892bdb863a2214664840451f9ac713
Kawser365 <mdkamrulhasan.khk@gmail.com>
2023-10-20 22:00:08 +0600

    [SOLVE]netlify routing issue

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,9 +4,11 @@ import Clients from '../../components/Clients/Clients'
 import Header from '../../components/Header/Header'
 import OurOffer from '../../components/OurOffer/OurOffer'
 import OurServices from '../../components/OurServices/OurServices'
+import PageTitle from '../../components/shared/PageTitle/PageTitle'
 const Home = () => {
   return (
     <div className='overflow-hidden'>
+      <PageTitle title="Digonto Soft | Home"></PageTitle>
       <Header></Header>
       <OurServices></OurServices>
       <Clients></Clients>
